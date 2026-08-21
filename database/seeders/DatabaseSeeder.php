0de6160033ce7cf5100af701a83631dc7b19c672
ziagit <zia.csco@gmail.com>
2026-08-21 23:08:03 +0500

    image updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,7 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create(['name' => 'Admin', 'email' => 'admin@ozghan.com.au', 'password' => 'password']);
+        User::updateOrCreate(
+            ['email' => 'admin@ozghan.com.au'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('password'),
+                'email_verified_at' => now(),
+            ]
+        );
         $this->call(SiteContentSeeder::class);
         $this->call(ServiceContentSeeder::class);
         $this->call(WorkImagesSeeder::class);
